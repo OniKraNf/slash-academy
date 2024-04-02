f8efccf69ade639b2d38f0e7815d4329d69e7522
parijdev <mrreptile22@yahoo.com>
2024-04-03 02:48:08 +0300

    feat: nevermind

--- webinars/urls.py
+++ webinars/urls.py
@@ -6,5 +6,5 @@ from .views import *
 urlpatterns = [
     path('', WebinarView.as_view(), name='webinars'),   
     path('web-test-page', web_test_page, name='web_test_page'),
-    path('webinars/<slug:slug>/', WebinarView.as_view(), name="webinar_detail")
+    path('<slug:slug>/', WebinarView.as_view(), name="webinar_detail")
 ]
